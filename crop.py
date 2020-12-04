3e17cd8a3cc11de925a49fa1ca3d040cc8ae2677
Pierre <pierre.marsaa@icloud.com>
2020-12-04 22:53:45 +0100

    le code marche enfin

--- crop.py
+++ crop.py
@@ -7,6 +7,7 @@ Created on Fri Dec  4 21:08:46 2020
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import moviepy.editor
 import glob 
+import os 
 
 def combine_audio(vidname, audname, outname): # fonction qui fusionne video et audio 
     import moviepy.editor as mpe
@@ -21,9 +22,9 @@ def crop_video(way,format_video) :
     
     for filename in glob.glob(way+"/*."+format_video) :  # on va parcourir toutes les videos du fichier
     
-        filename = filename.replace(way+"\\","")
-    
-        print("fichier : ", i, " nom : ", filename) # on print le numéro et la vidéo  
+        filename = filename
+        
+        print("fichier : ", i, " nom : ", filename.replace(way+"\\","")) # on print le numéro et le nom simplifié de la vidéo   
         list_file.append([i,filename]) # on ajoute le couple au tableau 
         i+=1 # on incrémente i 
     
@@ -33,17 +34,18 @@ def crop_video(way,format_video) :
     for j in range(len(list_file)) : # on parcoure la liste des vidéos pour voir si on a le numéro correspondant 
         if video_name == list_file[j][0] : # on compare numéro donné avec les numéros des vidéos 
             video_name = list_file[j][1] # on lui donne le nom de la vidéo correspondante 
-            print("Vidéo choisie : ",video_name)
+            print("Vidéo choisie : ",video_name.replace(way+"\\",""))
     
     # d'autres demande je pense t'as capté tavu
     time_start = float(input("Temps début (secondes) :\t"))
     time_stop = float(input("Temps fin (secondes) :\t"))
-    new_name = str("new_"+video_name)
-    
+    new_name = str(way+"\\new_"+video_name.replace(way+"\\",""))
     
     # on crop avec les infos données par l'utilisateur dans les inputs 
     ffmpeg_extract_subclip(video_name, time_start, time_stop, targetname=new_name)
     
     
 # mettre le chemin où il y a vos vidéos 
-crop_video(way = r"D:\Prog&Job\Business\En cours\TikTok\Quotes\movie_show_quotes\Croping_video", format_video = "mp4")
+#crop_video(way = r"D:\Prog&Job\Business\En cours\TikTok\Quotes\movie_show_quotes\Croping_video", format_video = "mp4")
+
+crop_video(way = r"D:\Prog&Job\Business\En cours\TikTok\Quotes\movie_show_quotes\Croping_video\movies\Alice au pays des merveilles", format_video = "mp4")
